c138bd75c6fac4cd14b4e27a57f1c4b4845c3360
mohammed-Emad <mohammed.imad38@yahoo.com>
2023-02-25 00:14:59 +0200

    Update FACENET.py

--- FACENET.py
+++ FACENET.py
@@ -20,6 +20,52 @@ margin = 32
 batch_size = 100
 seed = 666
 
+def Train(load_m ,data_dir = "data_algin"):
+            print("args.seed??",seed)
+
+            sess = load_m.sess
+            images_placeholder = load_m.images_placeholder
+            embeddings = load_m.embeddings 
+            phase_train_placeholder = load_m.phase_train_placeholder 
+            embedding_size = load_m.embedding_size 
+            
+            np.random.seed(seed=seed)
+
+
+            dataset = facenet.get_dataset(data_dir)
+
+            print("dataset type",type(dataset))
+            print(dataset)
+            # Check that there are at least one training image per class
+
+            print("dataset=",len(dataset))
+            paths, labels = facenet.get_image_paths_and_labels(dataset)
+            
+            print('Number of classes new: %d' % len(dataset))
+            print('Number of images new: %d' % len(paths))
+            
+            # Load the model
+            print('Loading feature extraction model')
+
+            # Get input and output tensors
+            # Run forward pass to calculate embeddings
+            print('Calculating features for images')
+            nrof_images = len(paths)
+            nrof_batches_per_epoch = int(math.ceil(1.0*nrof_images / batch_size))
+            emb_array = np.zeros((nrof_images, embedding_size))
+            for i in range(nrof_batches_per_epoch):
+                start_index = i*batch_size
+                end_index = min((i+1)*batch_size, nrof_images)
+                paths_batch = paths[start_index:end_index]
+                images = facenet.load_data(paths_batch, False, False, image_size)
+                feed_dict = { images_placeholder:images, phase_train_placeholder:False }
+                emb_array[start_index:end_index,:] = sess.run(embeddings, feed_dict=feed_dict)
+
+            print(type(emb_array),"~",type(labels))
+
+            # Create a list of class names
+            class_names = [ cls.name.replace('_', ' ') for cls in dataset]
+            return emb_array, labels, class_names
 
 def Embedding(img_list ,load_m):
             # Get input and output tensors
