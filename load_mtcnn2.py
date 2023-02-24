c4ff8439b3dbf42e9657ed271e68522d1f49633c
mohammed-Emad <mohammed.imad38@yahoo.com>
2023-02-24 11:54:10 +0200

    Update load_mtcnn2.py

--- load_mtcnn2.py
+++ load_mtcnn2.py
@@ -39,7 +39,7 @@ gpu_memory_fraction = 0.5
 
 class load_all(threading.Thread):
      def __init__(self,MODEL):
-         threading.Thread.__init__(self)
+         threading.Thread.__init__(self,MODEL)
          print('Loading models')
          with tf.Graph().as_default():
             gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_memory_fraction)
